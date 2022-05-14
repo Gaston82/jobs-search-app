4b9e58b9f1b9e93166961c84b979634a07916692
Gaston82 <cabezaartaza@hotmail.com>
2022-05-15 01:24:28 +0200

    add helpers functions

--- helpers/db-validators.js
+++ helpers/db-validators.js
@@ -1,22 +1,31 @@
-const Role = require("../models/role");
+const User = require("../models/user");
+const Rol = require("../models/role");
 
-const doesRoleValid = async (rol = "") => {
-  const existeRol = await Role.findOne({ rol });
+const doesRoleValid = async (role = "") => {
+  const existeRol = await Rol.findOne({ role });
   if (!existeRol) {
-    throw new Error(`El rol ${rol} no está registrado en la BD`);
+    throw new Error(`El rol ${role} no está registrado en la BD`);
   }
 };
 
-//Verificar si el correo existe
+//Verificar si el correo existe po el id
 
-const doesEmailExist = async (correo = "") => {
-  const existeEmail = await Usuario.findOne({ correo });
+const doesUserExistById = async (id) => {
+  const userExist = await User.findById(id);
+  if (!userExist) {
+    throw new Error(`User with id:  ${id} doesn't exist`);
+  }
+};
+
+const doesEmailExist = async (email = "") => {
+  const existeEmail = await User.findOne({ email });
   if (existeEmail) {
-    throw new Error(`El correo ${correo} ya existe`);
+    throw new Error(`Email ${email} already exist`);
   }
 };
 
 module.exports = {
   doesRoleValid,
   doesEmailExist,
+  doesUserExistById,
 };
