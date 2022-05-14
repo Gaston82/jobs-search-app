94e101c59ba8ad9bfd933a464164e43db1e04ee2
Gaston82 <cabezaartaza@hotmail.com>
2022-05-14 02:36:28 +0200

    add encrypt password

--- controllers/users.js
+++ controllers/users.js
@@ -1,14 +1,27 @@
+const bcryptjs = require("bcryptjs");
 const { response } = require("express");
 
+const User = require("../models/user");
+
 const usersGet = (req, res = response) => {
+  const { name, api_key } = req.query;
   res.json({
     msg: "get API",
+    name,
+    api_key,
   });
 };
 
-const usersPost = (req, res = response) => {
+const usersPost = async (req, res = response) => {
+  const { name, email, role, password } = req.body;
+  const user = new User({ name, email, role, password });
+
+  const salt = bcryptjs.genSaltSync();
+  user.password = bcryptjs.hashSync(password, salt);
+  await user.save();
   res.json({
     msg: "post API",
+    user,
   });
 };
 
@@ -19,8 +32,10 @@ const usersDelete = (req, res = response) => {
 };
 
 const usersPut = (req, res = response) => {
+  const { id } = req.params;
   res.json({
-    msg: "get API",
+    msg: "put API",
+    id,
   });
 };
 
