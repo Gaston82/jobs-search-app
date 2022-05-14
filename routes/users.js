7d7cddc01952f8b9c2017cd3d4166ae4b2966d42
Gaston82 <cabezaartaza@hotmail.com>
2022-05-15 01:26:38 +0200

    add update user validation and delete user validation

--- routes/users.js
+++ routes/users.js
@@ -7,7 +7,11 @@ const {
   usersPut,
   usersDelete,
 } = require("../controllers/users");
-const { doesEmailExist, doesRoleValid } = require("../helpers/db-validators");
+const {
+  doesEmailExist,
+  doesRoleValid,
+  doesUserExistById,
+} = require("../helpers/db-validators");
 const { checkFields } = require("../middlewares/check-fields");
 
 const router = Router();
@@ -17,11 +21,11 @@ router.get("/", usersGet);
 router.post(
   "/",
   [
-    check("email", "Email not valid").isEmail(),
+    check("name", "Name is mandatory").not().isEmpty(),
     check("password", "Password should have at least 6 characters").isLength({
       min: 6,
     }),
-    check("name", "Name is mandatory").not().isEmpty(),
+    check("email", "Email not valid").isEmail(),
     check("email").custom(doesEmailExist),
     //check("correo", "El correo no es válido").isEmail(),
     //check("rol", "No es un rol válido").isIn(["ADMIN_ROLE", "USER_ROLE"]),
@@ -31,8 +35,25 @@ router.post(
   usersPost
 );
 
-router.put("/:id", usersPut);
+router.put(
+  "/:id",
+  [
+    check("id", " id not valid").isMongoId(),
+    check("id").custom(doesUserExistById),
+    check("role").custom(doesRoleValid),
+    checkFields,
+  ],
+  usersPut
+);
 
-router.delete("/", usersDelete);
+router.delete(
+  "/:id",
+  [
+    check("id", " id not valid").isMongoId(),
+    check("id").custom(doesUserExistById),
+    checkFields,
+  ],
+  usersDelete
+);
 
 module.exports = router;
