5300e8e2112e3d45789fe022927ed669e650f9c1
Gaston82 <cabezaartaza@hotmail.com>
2022-05-16 04:15:56 +0200

    add user to job model

--- models/job.js
+++ models/job.js
@@ -1,6 +1,10 @@
 const { Schema, model } = require("mongoose");
 
 const JobSchema = Schema({
+  title: {
+    type: String,
+    required: [true, "Title is required"],
+  },
   city: {
     type: String,
     required: [true, "City is required"],
@@ -16,6 +20,18 @@ const JobSchema = Schema({
     type: String,
     required: [true, "Description is required"],
   },
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
 });
+
+JobSchema.methods.toJSON = function () {
+  const { __v, _id, status, user, ...data } = this.toObject();
+  //Ahora cada vez que cree un usuario aparecerá con uid en lugar de _id
+  data.uid = _id;
+  return data;
+};
 
 module.exports = model("Job", JobSchema);
